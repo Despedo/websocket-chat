7a43dbbb1255fe9f4ff8a29ad22508301157fde1
Oleksandr Vechirko <despedo@gmail.com>
2018-05-22 00:55:03 +0300

    Added lombock and message time

--- src/main/java/com/websocket/controller/ChatController.java
+++ src/main/java/com/websocket/controller/ChatController.java
@@ -7,12 +7,17 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 public class ChatController {
 
     @MessageMapping("/chat/message")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        chatMessage.addTime(time);
         return chatMessage;
     }
 
--- src/main/java/com/websocket/model/ChatMessage.java
+++ src/main/java/com/websocket/model/ChatMessage.java
@@ -1,5 +1,10 @@
 package com.websocket.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChatMessage {
     private MessageType type;
     private String content;
@@ -11,27 +16,7 @@ public class ChatMessage {
         LEAVE
     }
 
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
+    public void addTime(String time){
+        this.content = time + ": " +content;
     }
 }
